63ac58938a979e422a86a8df3e7fe930c07cff30
roytudelft <73598327+roytudelft@users.noreply.github.com>
2026-02-19 14:50:46 +0100

    Update __init__.py
    Added call to D4a module

--- spirack/__init__.py
+++ spirack/__init__.py
@@ -3,6 +3,7 @@ from .version import __version__
 from .spi_rack import SPI_rack
 
 from .D4_module import D4_module
+from .D4a_module import D4a_module
 from .D5a_module import D5a_module
 from .F1d_module import F1d_module
 from .S5i_module import S5i_module
